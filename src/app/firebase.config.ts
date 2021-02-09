79b5999e78dcd87f65b8b318d1378f09b7d91495
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2021-02-10 00:18:37 +0100

    move gitignore

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyD6hy295dWsq4LZxLNKLbTW2EJ-O7WgOVY',
-  authDomain: 'optc-world-records.firebaseapp.com',
-  projectId: 'optc-world-records',
-  storageBucket: 'optc-world-records.appspot.com',
-  messagingSenderId: '336978520266',
-  appId: '1:336978520266:web:0b9a8cdfb8f6ff9def6c33',
-};
